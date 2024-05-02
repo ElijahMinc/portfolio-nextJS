3af706b4be3d9a5ce23ab8a76725ad4a9a7847f8
Ilya Protsenko <ilya.protsenko@nixsolutions.com>
2024-05-02 11:23:03 +0100

    Changed speed of particles moving

--- src/shared/hoc/withParticles.tsx
+++ src/shared/hoc/withParticles.tsx
@@ -3,7 +3,64 @@ import { useCallback } from 'react';
 import Particles from 'react-particles';
 //import { loadFull } from "tsparticles"; // if you are going to use `loadFull`, install the "tsparticles" package too.
 import { loadSlim } from 'tsparticles-slim'; // if you are going to use `loadSlim`, install the "tsparticles-slim" package too.
-import type { Container, Engine } from 'tsparticles-engine';
+import type {
+  Container,
+  Engine,
+  RecursivePartial,
+  IOptions,
+} from 'tsparticles-engine';
+
+const particlesOptions: Partial<RecursivePartial<IOptions>> = {
+  zIndex: -1,
+  fpsLimit: 120,
+  interactivity: {
+    events: {
+      onHover: {
+        enable: true,
+        mode: 'repulse',
+      },
+    },
+  },
+  particles: {
+    color: {
+      value: '#ffffff',
+    },
+    links: {
+      color: '#ffffff',
+      distance: 150,
+      enable: true,
+      opacity: 0.5,
+      width: 1,
+    },
+    move: {
+      direction: 'none',
+      enable: true,
+      outModes: {
+        default: 'bounce',
+      },
+      random: false,
+      speed: 2,
+      straight: false,
+    },
+    number: {
+      density: {
+        enable: true,
+        area: 800,
+      },
+      value: 80,
+    },
+    opacity: {
+      value: 0.5,
+    },
+    shape: {
+      type: 'circle',
+    },
+    size: {
+      value: { min: 1, max: 5 },
+    },
+  },
+  detectRetina: true,
+};
 
 export const ParticlesLayout = ({
   children,
@@ -31,76 +88,7 @@ export const ParticlesLayout = ({
         id="tsparticles"
         init={particlesInit}
         loaded={particlesLoaded}
-        options={{
-          //  background: {
-          //    color: {
-          //      value: '#0d47a1',
-          //    },
-          //  },
-          zIndex: -1,
-          fpsLimit: 120,
-          interactivity: {
-            events: {
-            //   onClick: {
-            //     enable: true,
-            //     mode: 'push',
-            //   },
-              onHover: {
-                enable: true,
-                mode: 'repulse',
-              },
-            //   resize: true,
-            },
-            // modes: {
-            //   push: {
-            //     quantity: 4,
-            //   },
-            //   repulse: {
-            //     distance: 200,
-            //     duration: 0.4,
-            //   },
-            // },
-          },
-          particles: {
-            color: {
-              value: '#ffffff',
-            },
-            links: {
-              color: '#ffffff',
-              distance: 150,
-              enable: true,
-              opacity: 0.5,
-              width: 1,
-            },
-            move: {
-              direction: 'none',
-              enable: true,
-              outModes: {
-                default: 'bounce',
-              },
-              random: false,
-              speed: 6,
-              straight: false,
-            },
-            number: {
-              density: {
-                enable: true,
-                area: 800,
-              },
-              value: 80,
-            },
-            opacity: {
-              value: 0.5,
-            },
-            shape: {
-              type: 'circle',
-            },
-            size: {
-              value: { min: 1, max: 5 },
-            },
-          },
-          detectRetina: true,
-        }}
+        options={particlesOptions}
       />
     </>
   );
