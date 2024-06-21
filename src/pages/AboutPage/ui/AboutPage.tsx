a6123e15ced6d3079c7aa86e90aaad3e53a3cee7
Ilya Protsenko <ilya.protsenko@nixsolutions.com>
2024-06-21 17:56:19 +0100

    Returned about page

--- src/pages/AboutPage/ui/AboutPage.tsx
+++ src/pages/AboutPage/ui/AboutPage.tsx
@@ -10,10 +10,6 @@ import { imageQuality } from '@/shared/constants/images';
 import { useCursor } from '@/entities/Cursor';
 import { useTextAnimation } from '@/shared/hooks';
 import { withParticles } from '@/entities/Particles';
-import { motionImageWrapperAnimationConfig } from '../config/motion-animation';
-
-import cn from 'classnames';
-import './about-page.styles.css';
 
 const About = ({ aboutPage }: AboutPageProps) => {
   const titleRef = useRef(null);
@@ -34,21 +30,23 @@ const About = ({ aboutPage }: AboutPageProps) => {
       animate={{ opacity: 1 }}
       exit={{ opacity: 0 }}
       transition={transition1}
-      className="aboutpage section"
+      className="section  lg:pt-[150px] pt-0 "
     >
-      <div className="aboutpage__cover" />
+      <div className="dark:bg-black bg-white absolute top-0 left-0 w-full h-full pointer-events-none opacity-50 block lg:hidden z-10" />
 
-      <div className="aboutpage__container">
+      <div className="container mx-auto relative ">
         {/* text and img wrapepr */}
 
-        <div className="aboutpage__body">
+        <div className="flex flex-col lg:flex-row h-full items-center justify-center gap-x-24 text-center lg:text-left lg:pt-16 ">
           {personImg && (
             <motion.div
-              {...motionImageWrapperAnimationConfig}
+              initial={{ opacity: 0, x: '-50%' }}
+              animate={{ opacity: 1, x: 0 }}
+              exit={{ opacity: 0, x: '-50%' }}
               onMouseEnter={mouseEnterHandle}
               onMouseLeave={mouseLeaveHandle}
               transition={transition1}
-              className="aboutpage__image-wrapper"
+              className="absolute flex-1 max-h-96 lg:max-h-full order-2 lg:order-none overflow-hidden lg:relative lg:blur-none top-10%  left-50% lg:left-0 lg:w-[500px] lg:h-[500px] w-[60%] h-[60%] blur-sm pointer-events-none lg:pointer-events-auto"
             >
               <Image
                 quality={imageQuality}
@@ -64,7 +62,7 @@ const About = ({ aboutPage }: AboutPageProps) => {
           <div
             onMouseEnter={mouseEnterHandle}
             onMouseLeave={mouseLeaveHandle}
-            className="aboutpage__content"
+            className="flex-1 pt-36 pb-14 lg:pt-0 lg:w-auto z-10 flex flex-col justify-center items-center lg:items-start"
           >
             {title && (
               <h1 className="h1" ref={titleRef}>
@@ -74,13 +72,13 @@ const About = ({ aboutPage }: AboutPageProps) => {
 
             {subtitle && (
               <div
-                className="aboutpage__subtitle"
+                className="mb-12 max-w-sm flex flex-col gap-2 dark:text-white text-black"
                 dangerouslySetInnerHTML={{ __html: subtitle }}
               />
             )}
 
             <Link
-              className={cn('aboutpage__btn', 'btn-fancy')}
+              className="btn-fancy mb-[30px] hover:rounded-tr-lg hover:rounded-bl-lg hover:tracking-widest"
               href={ROUTES.PORTFOLIO}
             >
               View my work
